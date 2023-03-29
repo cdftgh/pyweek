f46e99e67134e2b52b7265d5a48a732a9a55cba6
Shahar Sarfati <99743592+cdftgh@users.noreply.github.com>
2023-03-29 20:49:36 +0300

    gem

--- game/screens/spaceship.py
+++ game/screens/spaceship.py
@@ -2,6 +2,8 @@
 import pygame
 import random
 
+pi = 3.141592653589793238462643383279502884197169399375105820974944592307816406286208998628034825342117067
+
 class GameScreen:
     def __init__(self, width, height):
         self.width = width
@@ -11,11 +13,12 @@ class GameScreen:
 
         self.arr = []
         self.backgrounds=[]
-        self.background = pygame.image.load("images/first_room.png")
+
+        self.background = pygame.image.load(r"pyweek\game\images\first_room.png")
         self.backgrounds.append(self.background)
         self.background = pygame.transform.scale(self.background, (width, height))
 
-        key = pygame.image.load("images/key.png")
+        key = pygame.image.load(r"pyweek\game\images\key.png")
         self.key = pygame.transform.scale(key, (25, 30))
         self.battery=10000000
         self.used=0
@@ -24,15 +27,23 @@ class GameScreen:
         self.gotkey=0
         self.widthkey=25
         self.heightkey=30
+
+        powerbox = pygame.image.load(r"pyweek\game\images\power_box.png")
+        self.powerbox = pygame.transform.scale(powerbox, (160, 200))
+
+        self.deathText = pygame.image.load(r"pyweek\game\images\death_text.png")
+        self.deathText = pygame.transform.scale(self.deathText, (604, 480))
+
     def display(self,lighton,radius):
         self.screen.fill([0, 0, 0])
 
+        a, b = pygame.mouse.get_pos()
         # put objects onto the background before calling array3d()
         if not self.gotkey:
             self.background.blit(self.key, (self.xkey, self.ykey))
+            self.background.blit(self.powerbox, (320, 180))
+            self.clickedkey(a,b)
 
-        a, b = pygame.mouse.get_pos()
-        self.clickedkey(a,b)
         color = (255*self.used/self.battery, 255*(1-self.used/self.battery), 0)
 
         # Drawing Rectangle
@@ -45,8 +56,11 @@ class GameScreen:
 
         #if a < self.arr.shape[0] - self.radius and b < self.arr.shape[1] + self.radius:
         if lighton and random.random()<0.95 and self.used/self.battery<1:
-            self.used+=1*radius*radius*3.14159265358979323
+            self.used+=1*radius*radius*pi
             self.see(self.screen, a, b,radius, self.arr)
+            
+        elif self.used/self.battery>=1:
+            self.screen.blit(self.deathText, (0, 0))
 
     def see(self, screen, a, b, radius, pixels):
         for i in range(max(0,a - radius),min(self.arr.shape[0], a + radius)):
@@ -58,5 +72,6 @@ class GameScreen:
     def clickedkey(self,a,b):
         if a>self.xkey and a<self.widthkey+self.xkey and b>self.ykey and b<self.heightkey+self.ykey:
             self.gotkey=1
-            self.background = pygame.image.load("images/first_room.png")
+            self.background = pygame.image.load(r"pyweek\game\images\first_room.png")
             self.background = pygame.transform.scale(self.background, (self.width, self.height))
+            self.used /= 2
